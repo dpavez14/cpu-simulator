274d7edeb2df0442f064c5a506b4ee050c83e0ef
Daniel Pavez <dpavez14@alumnos.utalca.cl>
2021-07-01 21:50:46 -0400

    Init angular project

---
+++ src/app/procesador/procesador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-procesador',
+  templateUrl: './procesador.component.html',
+  styleUrls: ['./procesador.component.scss']
+})
+export class ProcesadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
